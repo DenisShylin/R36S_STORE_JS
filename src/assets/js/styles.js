f898b1e15932e88253ae8c6ba46e1ffc10314162
Shylin Denis <tawnship406@gmail.com>
2025-03-20 10:55:33 +0200

    import  через styles.js

--- src/assets/js/styles.js
+++ src/assets/js/styles.js
@@ -1,24 +1,24 @@
-// // src/assets/js/styles.js
-// // Централизованный импорт всех стилей проекта
+// src/assets/js/styles.js
+// Централизованный импорт всех стилей проекта
 
-// // Основные стили
-// import '../../index.css';
+// Основные стили
+import '../../index.css';
 
-// // Стили секций
-// import '../../sections/Hero/Hero.css';
-// import '../../sections/Header/Header.css';
-// import '../../sections/About/About.css';
-// import '../../sections/Articles/Articles.css';
-// import '../../sections/Categories/Categories.css';
-// import '../../sections/Contact/Contact.css';
-// import '../../sections/Features/Features.css';
-// import '../../sections/Footer/Footer.css';
-// import '../../sections/Products/Products.css';
-// import '../../sections/Reviews/Reviews.css';
+// Стили секций
+import '../../sections/Hero/Hero.css';
+import '../../sections/Header/Header.css';
+import '../../sections/About/About.css';
+import '../../sections/Articles/Articles.css';
+import '../../sections/Categories/Categories.css';
+import '../../sections/Contact/Contact.css';
+import '../../sections/Features/Features.css';
+import '../../sections/Footer/Footer.css';
+import '../../sections/Products/Products.css';
+import '../../sections/Reviews/Reviews.css';
 
-// import '../../sections/About/ModalAbout/ModalAbout.css';
-// import '../../sections/Footer/PartnershipModal/PartnershipModal.css';
+import '../../sections/About/ModalAbout/ModalAbout.css';
+import '../../sections/Footer/PartnershipModal/PartnershipModal.css';
 
-// import '../../components/MobileMenu/MobileMenu.css';
+import '../../components/MobileMenu/MobileMenu.css';
 
-// console.log('Стили загружены');
+console.log('Стили загружены');
